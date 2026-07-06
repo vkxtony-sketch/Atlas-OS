952cd60d89ad6c693961cddd8cbafa724713b560
vkxtony-sketch <vkxtony@gmail.com>
2026-07-06 14:47:11 -0400

    Add core package initialization for Atlas OS

---
+++ core/__init__.py
@@ -0,0 +1,9 @@
+"""
+Atlas OS Core Package
+"""
+
+from .task import Task
+from .memory import Memory
+from .consensus_engine import ConsensusEngine
+from .executor import Executor
+from .executive_ai import ExecutiveAI
