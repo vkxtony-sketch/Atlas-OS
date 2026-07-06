97eb4739bd8c7bf4656ff326dfa2677999adafc2
vkxtony-sketch <vkxtony@gmail.com>
2026-07-06 14:47:15 -0400

    Add agents package initialization for Atlas OS

---
+++ agents/__init__.py
@@ -0,0 +1,8 @@
+"""
+Atlas OS Agents Package
+"""
+
+from .planner_agent import PlannerAgent
+from .research_agent import ResearchAgent
+from .coding_agent import CodingAgent
+from .critic_agent import CriticAgent
